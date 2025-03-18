1e0ca175d4dbfdb25fc4ce6a319943615ec8f95b
vivek31122002 <136363672+vivek31122002@users.noreply.github.com>
2025-03-18 13:12:37 +0530

    Save My commit

--- Week1/Basics.java
+++ Week1/Basics.java
@@ -3,6 +3,17 @@ package Week1;
 public class Basics {
 
     public static void main(String[] args) {
+
         System.out.println("Hello World");
+        printNumbers(0);
+    }
+
+    public static void printNumbers(int n){
+        if(n==5){
+            System.out.println(n);
+            return;
+        }
+        printNumbers(n+1);
+        System.out.println(n);
     }
 }
