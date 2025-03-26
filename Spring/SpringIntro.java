b6ef2735919373fa15afa27713cd8049fd517818
VivekRkIvoyant <vivekr@ivoyant.com>
2025-03-26 13:01:37 +0530

    spring intro

---
+++ Spring/SpringIntro.java
@@ -0,0 +1,12 @@
+package Spring;
+
+public class SpringIntro {
+
+    public static void main(String[] args) {
+        System.out.println("This is introduction to Java Spring\n");
+        System.out.println("Spring uses:\n" +
+                "DI-(Dependency Injection)\n" +
+                "POJO-(Plain Old Java Object)\n" +
+                "AOP-(Aspect Oriented Programming)\n");
+    }
+}
